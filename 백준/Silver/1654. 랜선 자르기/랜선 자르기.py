a8aef642fe411498c2b9823ea64d7b53a150f58a
yoonjeong <101466284+NYOONJEONG@users.noreply.github.com>
2024-01-31 02:30:54 +0900

    [Silver II] Title: 랜선 자르기, Time: 480 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -0,0 +1,19 @@
+K, N = map(int, input().split())
+height = []
+for _ in range(K):
+    height.append(int(input()))
+
+start = 1
+end = max(height)
+output= 0
+while start <= end : 
+    total =0
+    mid = (start+end)//2
+    for x in height:
+         total += x//mid
+    if total >=  N :
+        start = mid +1
+    else :
+        end = mid-1
+
+print(end)
