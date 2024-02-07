d7b5e5c7e7cd1fee3c1c1837ca915e66b35d225a
yoonjeong <101466284+NYOONJEONG@users.noreply.github.com>
2024-02-07 11:11:38 +0900

    [Silver III] Title: 주유소, Time: 116 ms, Memory: 45892 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -2,10 +2,10 @@ N = int(input())
 road = list(map(int, input().split()))
 station = list(map(int, input().split()))
 cost = 0
-for i in range(N-1):
-    if station[i]==min(station[i:N-1]) :
-        cost += station[i] * sum(road[i:N-1]) 
-        break
-    else: cost += station[i] * road[i]
+min_station = station[0]
 
+for i in range(N-1):
+    if station[i] < min_station :
+        min_station = station[i]
+    cost += min_station * road[i]
 print(cost)
