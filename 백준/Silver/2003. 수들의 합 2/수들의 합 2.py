441e0c77ace2a62a3cd1cce21f20bdd616dcbc40
yoonjeong <101466284+NYOONJEONG@users.noreply.github.com>
2024-04-20 15:05:31 +0900

    [Silver IV] Title: 수들의 합 2, Time: 44 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.py
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.py
@@ -3,13 +3,13 @@ input = sys.stdin.readline
 n,m= map(int,input().split())
 num = list(map(int,input().split()))
 cnt = 0
-cumsum, right = 0,0
-for i in range(n):
-    while cumsum <m and right <n:
-        cumsum += num[right]
-        right += 1
+cumsum, end = 0,0
+for start in range(n):
+    while cumsum <m and end <n:
+        cumsum += num[end]
+        end += 1
     if cumsum==m :
         cnt+=1
-    cumsum -= num[i]
+    cumsum -= num[start]
 
 print(cnt)
