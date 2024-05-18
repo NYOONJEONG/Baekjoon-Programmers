f08dab59db73f61a8fbee1e0e416c5d06032fa7c
yoonjeong <101466284+NYOONJEONG@users.noreply.github.com>
2024-05-18 15:42:35 +0900

    [Gold IV] Title: 이진 검색 트리, Time: 2928 ms, Memory: 424776 KB -BaekjoonHub

--- 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
@@ -1,38 +1,30 @@
 import sys
 input = sys.stdin.readline
-#recursion error 방지
 sys.setrecursionlimit(10**9)
 
-arr = []
+tree = []
 while True:
-    try:
-        x = int(input())
-        arr.append(x)
-    except:
+    try :
+        x= int(input())
+        tree.append(x)
+    except :
         break
 
-
-def solution(A):
-    # 받은 배열 길이가 0이면 return
-    if len(A) == 0:
+def find_tree(Tree):
+    if len(Tree)==0:
         return
+    L,R= [], []
+    mid = Tree[0]
 
-    tempL, tempR = [], []
-    # 첫번째 값을 루트 노드로 설정
-    mid = A[0]
-    # 나머지 배열에 대해 for문을 돌면서 루트보다 커지는 부분을 기록해서 L과 R로 나눈다.
-    for i in range(1, len(A)):
-        if A[i] > mid:
-            tempL = A[1:i]
-            tempR = A[i:]
+    for i in range(1,len(Tree)):
+        if Tree[i]>mid :
+            L= Tree[1:i]
+            R = Tree[i:]
             break
-    else:
-    	#모두 mid보다 작은 경우
-        tempL = A[1:]
-    
-    #왼쪽, 오른쪽 순으로 재귀 후 루트 노드 값 출력
-    solution(tempL)
-    solution(tempR)
+    else :
+        L = Tree[1:]
+    find_tree(L)
+    find_tree(R)
     print(mid)
-
-solution(arr)
+    
+find_tree(tree)
