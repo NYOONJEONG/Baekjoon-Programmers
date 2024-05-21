08c30508fc9c2229b52bc4b51a3c9bab01f04cd4
yoonjeong <101466284+NYOONJEONG@users.noreply.github.com>
2024-05-21 13:30:43 +0900

    [Gold IV] Title: 거짓말, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/1043. 거짓말/거짓말.py
@@ -0,0 +1,21 @@
+import sys
+input= sys.stdin.readline
+n, m = map(int, input().split())
+truth = set(input().split()[1:])
+
+party = []
+for _ in range(m):
+    party.append(set(input().split()[1:]))
+
+for _ in range(m):
+    for p in party:
+        if p & truth:
+            truth = truth.union(p) 
+
+cnt = 0
+for p in party:
+    if p & truth :
+        continue
+    else:
+        cnt +=1
+print(cnt)
