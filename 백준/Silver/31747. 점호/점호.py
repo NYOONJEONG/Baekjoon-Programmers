ca9543c631fa0d7a5bcb8e099a9546e67d20d2aa
yoonjeong <101466284+NYOONJEONG@users.noreply.github.com>
2024-06-09 19:20:03 +0900

    [Silver III] Title: 점호, Time: 136 ms, Memory: 32684 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/31747. 점호/점호.py
+++ 백준/Silver/31747. 점호/점호.py
@@ -1,32 +1,36 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
 n,k = map(int,input().split())
 a = list(map(int,input().split()))
-d = deque(a)
-t = 0
-while d:
-    p = len(d)
-    m = min(p,k)
-    # print(m)
-    one, two = False, False
-    plus = []
-    # print(d,'0')
-    t += 1
 
-    for _ in range(m):
-        x = d.popleft()
-        if x ==1 and one==False:
-            one=True
-            # print(d,'1')
+cnt = {1:0, 2:0}
+for x in a[:k]:
+    cnt[x] += 1
+if n==k :
+    print(max(cnt[1], cnt[2]))
+else :
+    t = 0
+    start = 0
+    while start <= n:
+        if min(cnt[1],cnt[2])==0:
+            if cnt[1] ==0:
+                cnt[2] -= 1
+            else:
+                cnt[1] -= 1
             
-        elif x==2 and two == False:
-            two = True
-            # print(d,'2')
-        
-        elif one == True or two == True:
-            plus.append(x)
+            if start + k < n:
+                cnt[a[start+k]] += 1
+            
+            start += 1
 
-    d.extendleft(plus)
-    # print(d, '3', t)
-print(t)
+        else :
+            cnt[1] -= 1
+            cnt[2] -= 1
+            if start +k <n :
+                cnt[a[start+k]] += 1
+            if start + k + 1 < n:
+                cnt[a[start+k+1]] += 1
+        
+            start += 2
+        t += 1
+    print(t-1)
