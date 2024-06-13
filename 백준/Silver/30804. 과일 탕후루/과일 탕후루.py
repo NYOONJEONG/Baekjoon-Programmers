c4d0765da8c35a3683a751113e195b580761397a
yoonjeong <101466284+NYOONJEONG@users.noreply.github.com>
2024-06-13 12:22:30 +0900

    [Silver II] Title: 과일 탕후루, Time: 216 ms, Memory: 34600 KB -BaekjoonHub

--- 백준/Silver/30804. 과일 탕후루/과일 탕후루.py
+++ 백준/Silver/30804. 과일 탕후루/과일 탕후루.py
@@ -6,27 +6,25 @@ n = int(input())
 S = list(map(int,input().split()))
 num = [0] * (max(S)+1)
 
-def fruit(start, end, cnt, kind, max_val):
-    global num
-
-    if end >= n:
-        return max_val 
-
-    if num[S[end]] == 0:
-        kind += 1
-
-    cnt += 1 
-    num[S[end]] += 1 
+def fruit():
+    start, end, cnt, kind, max_val = 0, 0, 0, 0, 0
+    while end < n:
+        if num[S[end]] == 0:
+            kind += 1
+        
+        cnt += 1
+        num[S[end]] += 1
+        
+        while kind > 2:
+            if num[S[start]] == 1:
+                kind -= 1
+            num[S[start]] -= 1
+            cnt -= 1
+            start += 1
+        
+        max_val = max(max_val, cnt)
+        end += 1
     
-    if kind > 2: 
-        if num[S[start]] == 1: 
-            kind -= 1
-        num[S[start]] -= 1
-        cnt -= 1 
-        start += 1 
+    return max_val
 
-    max_val = max(max_val, cnt) 
-
-    return fruit(start, end + 1, cnt, kind, max_val) 
-
-print(fruit(0, 0, 0, 0, 0))
+print(fruit())
