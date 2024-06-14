2be0be2dec363b0aa59173ebe033a60a34793fbc
yoonjeong <101466284+NYOONJEONG@users.noreply.github.com>
2024-06-14 11:41:39 +0900

    [Silver IV] Title: 이삿짐센터, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/16237. 이삿짐센터/이삿짐센터.py
@@ -0,0 +1,33 @@
+a,b,c,d,e = map(int,input().split())
+
+min_box = e+d
+a -= d
+
+while c > 0:
+    min_box += 1
+    if b > 0:
+        b -= 1
+        c -= 1
+    elif a > 0:
+        a -= 2
+        c -= 1
+    else:
+        c -= 1
+
+while b > 0:
+    min_box += 1
+    if b > 1 and a > 0:
+        b -= 2
+        a -= 1
+    elif b > 0 and a > 0:
+        b -= 1
+        a -= 3
+    else:
+        b -= 2
+
+
+while a > 0:
+    min_box += 1
+    a -= 5
+
+print(min_box)
