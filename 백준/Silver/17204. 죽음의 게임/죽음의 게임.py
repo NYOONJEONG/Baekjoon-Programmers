8771cc2b9ba7b35ea983215861510f18f5638323
yoonjeong <101466284+NYOONJEONG@users.noreply.github.com>
2024-06-27 12:16:57 +0900

    [Silver III] Title: 죽음의 게임, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/17204. 죽음의 게임/죽음의 게임.py
@@ -0,0 +1,13 @@
+n , k = map(int,input().split())
+number = [int(input()) for _ in range(n)]
+arr = []
+flag = 0
+cnt = 0
+for i in range(n):
+    flag = number[flag]
+    cnt += 1
+    if flag == k:
+        break
+if cnt != n:
+    print(cnt)
+else : print(-1)
